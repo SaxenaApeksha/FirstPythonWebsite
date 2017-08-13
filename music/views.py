49241787234e801c99367a80263b2311fca538b6
SaxenaApeksha <net.apeksha@gmail.com>
2017-08-13 13:19:58 +0530

    First Commit for Python

---
+++ music/views.py
@@ -0,0 +1,30 @@
+from audioop import alaw2lin
+from distutils.command.register import register
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,get_object_or_404
+from .models import Album,Song
+
+def index(request):
+    all_album = Album.objects.all()
+    return render(request,'music/index.html',{ 'all_album':all_album})
+
+def detail(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render (request, 'music/detail.html', {'album':album})
+
+def favorite(request,album_id):
+    album = get_object_or_404 (Album, pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except ( KeyError, Song.DoesNotExist):
+        return render (request, 'music/detail.html', {
+            'album': album,
+            'error_message':"You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render (request, 'music/detail.html', {'album': album})
